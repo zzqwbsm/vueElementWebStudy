7fc562276a5a7ccc682f10cfc8f0eafcd0c01095
zengziqian <zengziqian@syswin.com>
2018-05-30 18:14:10 +0800

    项目上传-zzq

---
+++ src/components/common/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="wrapper">
+        <v-head></v-head>
+        <v-sidebar></v-sidebar>
+        <div class="content-box" :class="{'content-collapse':collapse}">
+            <v-tags></v-tags>
+            <div class="content">
+                <transition name="move" mode="out-in">
+                    <keep-alive>
+                        <router-view></router-view>
+                    </keep-alive>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import vHead from './Header.vue';
+    import vSidebar from './Sidebar.vue';
+    import vTags from './Tags.vue';
+    import bus from '../common/bus';
+    export default {
+        data(){
+            return {
+                collapse: false
+            }
+        },
+        components:{
+            vHead, vSidebar, vTags
+        },
+        created(){
+            bus.$on('collapse', msg => {
+                this.collapse = msg;
+            })
+        }
+    }
+</script>
